b7c3e7ea5bfd55145a8d57f4550eec6f15ee9426
suddenly <suddenlybydl0@gmail.com>
2022-09-18 10:45:12 +0500

    Fixed draw_line and draw_poly_line.

--- src/draw-list/draw-list.cpp
+++ src/draw-list/draw-list.cpp
@@ -204,7 +204,7 @@ namespace null::render {
         }
 
         std::ranges::for_each(std::views::iota(0, num_segments + 1), [=](const int& i) {
-            const float a = a_min + ((float)i / (float)num_segments) * (a_max - a_min);
+            const float a{ a_min + ((float)i / (float)num_segments) * (a_max - a_min) };
             path.push_back(center + vec2_t{ cosf(a), sinf(a) } *radius);
             });
     }
@@ -237,7 +237,7 @@ namespace null::render {
         if(points.size() < 3 || color.a() <= 0.f) return;
 
         if(parent_shared_data->initialize_flags & e_draw_list_flags::anti_aliased_fill) {
-            static constexpr float aa_size = 1.f;
+            static constexpr float aa_size{ 1.f };
             std::ranges::for_each(std::views::iota(2, (int)points.size()), [=](const int& i) {
                 prim_insert_idx({
                     (std::uint16_t)(vtx_buffer.size()), (std::uint16_t)(vtx_buffer.size() + ((i - 1) << 1)), (std::uint16_t)(vtx_buffer.size() + (i << 1))
@@ -245,19 +245,19 @@ namespace null::render {
                 });
 
             std::vector<vec2_t> temp_normals(points.size());
-            for(int i0 = points.size() - 1; int i1 : std::views::iota((size_t)0, points.size())) {
+            for(int i0{ (int)points.size() - 1 }; int i1 : std::views::iota((size_t)0, points.size())) {
                 vec2_t delta = points[i1] - points[i0];
-                if(delta.length() > 0.f) delta *= 1.f / delta.length(); //@todo: add this in vec2_t methods
+                if(float d2{ std::powf(delta.length(), 2) }; d2 > 0.f) delta *= 1.f / std::sqrtf(d2);
 
                 temp_normals[i0] = { delta.y, -delta.x };
                 i0 = i1;
             }
 
-            size_t idx = vtx_buffer.size();
-            for(int i0 = points.size() - 1; int i1 : std::views::iota((size_t)0, points.size())) {
-                vec2_t delta = (temp_normals[i0] + temp_normals[i1]) / 2;
-                delta *= std::min(1.f / std::powf(delta.length(), 2), 100.f); //@todo: add this in vec2_t methods
-                delta *= aa_size;
+            size_t idx{ vtx_buffer.size() };
+            for(int i0{ (int)points.size() - 1 }; int i1 : std::views::iota((size_t)0, points.size())) {
+                vec2_t delta = (temp_normals[i0] + temp_normals[i1]) / 2.f;
+                if(float d2{ std::powf(delta.length(), 2) }; d2 > 0.000001f) delta *= 1.f / std::min(d2, 100.f);
+                delta *= aa_size / 2.f;
 
                 prim_insert_idx({
                     (std::uint16_t)(idx + (i1 << 1)),       (std::uint16_t)(idx + (i0 << 1)),       (std::uint16_t)(idx + 1 + (i0 << 1)),
@@ -279,30 +279,30 @@ namespace null::render {
     void c_draw_list::draw_poly_line(const std::vector<vec2_t>& points, const color_t& color, bool closed, float thickness) {
         if(points.size() < 2 || color.a() <= 0.f) return;
 
-        const int count = closed ? points.size() : points.size() - 1;
-        const bool thick_line = (thickness > 1.0f);
+        const int count{ int(closed ? points.size() : points.size() - 1) };
+        const bool thick_line{ thickness > 1.0f };
 
         if(parent_shared_data->initialize_flags & e_draw_list_flags::anti_aliased_lines) {
-            static constexpr float aa_size = 1.0f;
+            static constexpr float aa_size{ 1.0f };
 
             thickness = std::max(thickness, 1.0f);
 
-            const bool use_texture = (parent_shared_data->initialize_flags & e_draw_list_flags::anti_aliased_lines_use_texture) && ((int)thickness < 63) && (thickness - (int)thickness <= 0.00001f);
+            const bool use_texture{ (parent_shared_data->initialize_flags & e_draw_list_flags::anti_aliased_lines_use_texture) && ((int)thickness < 63) && (thickness - (int)thickness <= 0.00001f) };
 
             std::vector<vec2_t> temp_normals(points.size() * (use_texture || !thick_line ? 3 : 5));
             std::vector<vec2_t> temp_points(temp_normals.size() + points.size());
 
             for(int i1 : std::views::iota(0, count)) {
-                vec2_t delta = points[i1 + 1 == points.size() ? 0 : i1 + 1] - points[i1];
-                if(delta.length() > 0.f) delta *= 1.f / delta.length();
-
+                vec2_t delta{ points[i1 + 1 == points.size() ? 0 : i1 + 1] - points[i1] };
+                if(float d2{ std::powf(delta.length(), 2) }; d2 > 0.f) delta *= 1.f / std::sqrtf(d2);
                 temp_normals[i1] = { delta.y, -delta.x };
             }
 
-            if(!closed) *std::prev(temp_normals.end()) = *std::prev(temp_normals.end(), 2);
+            if(!closed) //*std::prev(temp_normals.end(), points.size() - 1) = *std::prev(temp_normals.end(), points.size() - 2);
+                temp_normals[points.size() - 1] = temp_normals[points.size() - 2];
 
             if(use_texture || !thick_line) {
-                const float half_draw_size = use_texture ? thickness * 0.5f + 1 : aa_size;
+                const float half_draw_size{ use_texture ? thickness * 0.5f + 1 : aa_size };
                 if(!closed) {
                     temp_points[0] = points.front() + temp_normals.front() * half_draw_size;
                     temp_points[1] = points.front() - temp_normals.front() * half_draw_size;
@@ -310,14 +310,14 @@ namespace null::render {
                     temp_points[(points.size() - 1) * 2 + 1]    = points.back() - temp_normals[points.size() - 1] * half_draw_size;
                 }
 
-                size_t idx = vtx_buffer.size();
+                size_t idx{ vtx_buffer.size() };
                 for(int i1 : std::views::iota(0, count)) {
-                    const bool last_point = i1 + 1 == points.size();
-                    const int i2 = last_point ? 0 : (i1 + 1);
-                    const unsigned int _idx = last_point ? vtx_buffer.size() : (idx + (use_texture ? 2 : 3));
+                    const bool last_point{ i1 + 1 == points.size() };
+                    const int i2{ last_point ? 0 : (i1 + 1) };
+                    const std::uint32_t _idx{ std::uint32_t(last_point ? vtx_buffer.size() : (idx + (use_texture ? 2 : 3))) };
 
-                    vec2_t delta = (temp_normals[i1] + temp_normals[i2]) / 2;
-                    delta *= std::min(1.0f / std::powf(delta.length(), 2), 100.f);
+                    vec2_t delta{ (temp_normals[i1] + temp_normals[i2]) / 2.f };
+                    if(float d2{ std::powf(delta.length(), 2) }; d2 > 0.000001f) delta *= 1.f / std::min(d2, 100.f);
                     delta *= half_draw_size;
 
                     temp_points[i2 * 2] = points[i2] + delta;
@@ -341,7 +341,7 @@ namespace null::render {
                 }
 
                 if(use_texture) {
-                    const rect_t& tex_uvs = parent_shared_data->font->container_atlas->texture.uv_lines[(int)thickness];
+                    const rect_t& tex_uvs{ parent_shared_data->font->container_atlas->texture.uv_lines[(int)thickness] };
                     for(int i : std::views::iota((size_t)0, points.size())) {
                         prim_insert_vtx({
                             { temp_points[i * 2],       tex_uvs.min, color },
@@ -358,7 +358,7 @@ namespace null::render {
                     }
                 }
             } else {
-                const float half_inner_thickness = (thickness - aa_size) * 0.5f;
+                const float half_inner_thickness{ (thickness - aa_size) * 0.5f };
 
                 if(!closed) {
                     temp_points[0] = points.front() + temp_normals.front() * (half_inner_thickness + aa_size);
@@ -371,14 +371,14 @@ namespace null::render {
                     temp_points[(points.size() - 1) * 4 + 3]    = points.back() - temp_normals[(points.size() - 1)] * (half_inner_thickness + aa_size);
                 }
 
-                size_t idx = vtx_buffer.size();
+                size_t idx{ vtx_buffer.size() };
                 for(int i1 : std::views::iota(0, count)) {
-                    const bool last_point = i1 + 1 == points.size();
-                    const int i2 = last_point ? 0 : (i1 + 1);
-                    const unsigned int _idx = last_point ? vtx_buffer.size() : (idx + 4);
+                    const bool last_point{ i1 + 1 == points.size() };
+                    const int i2{ last_point ? 0 : (i1 + 1) };
+                    const std::uint32_t _idx{ std::uint32_t(last_point ? vtx_buffer.size() : (idx + 4)) };
 
-                    vec2_t delta = (temp_normals[i1] + temp_normals[i2]) / 2;
-                    delta *= std::min(1.f / std::powf(delta.length(), 2), 100.f);
+                    vec2_t delta{ (temp_normals[i1] + temp_normals[i2]) / 2.f };
+                    if(float d2{ std::powf(delta.length(), 2) }; d2 > 0.000001f) delta *= 1.f / std::min(d2, 100.f);
                     vec2_t out = delta * (half_inner_thickness + aa_size), in = delta * half_inner_thickness;
                     temp_points[i2 * 4]     = points[i2] + out;
                     temp_points[i2 * 4 + 1] = points[i2] + in;
@@ -408,10 +408,10 @@ namespace null::render {
             }
         } else {
             for(int i1 : std::views::iota(0, count)) {
-                const int i2 = i1 + 1 == points.size() ? 0 : (i1 + 1);
-                vec2_t delta = points[i2] - points[i1];
-                if(delta.length() > 0.f) delta *= 1.f / delta.length();
-                delta *= thickness / 2;
+                const int i2{ i1 + 1 == points.size() ? 0 : (i1 + 1) };
+                vec2_t delta{ points[i2] - points[i1] };
+                if(float d2{ std::powf(delta.length(), 2) }; d2 > 0.f) delta *= 1.f / std::sqrtf(d2);
+                delta *= thickness / 2.f;
 
                 prim_insert_idx({
                     (std::uint16_t)vtx_buffer.size(), (std::uint16_t)(vtx_buffer.size() + 1), (std::uint16_t)(vtx_buffer.size() + 2),
@@ -433,9 +433,8 @@ namespace null::render {
 
         parent_shared_data->get_auto_circle_num_segments(num_segments, radius);
 
-        const float a_max = (std::numbers::pi * 2.f) * (num_segments - 1.f) / num_segments;
         if(num_segments == 12) path_arc_to_fast(center, radius - 0.5f, 0, 11);
-        else path_arc_to(center, radius - 0.5f, 0.f, a_max, num_segments - 1);
+        else path_arc_to(center, radius - 0.5f, 0.f, (std::numbers::pi * 2.f) * (num_segments - 1.f) / num_segments, num_segments - 1);
         path_stroke(clr, true, thickness);
     }
 
@@ -444,9 +443,8 @@ namespace null::render {
 
         parent_shared_data->get_auto_circle_num_segments(num_segments, radius);
 
-        const float a_max = (std::numbers::pi * 2.f) * (num_segments - 1.f) / num_segments;
         if(num_segments == 12) path_arc_to_fast(center, radius, 0, 11);
-        else path_arc_to(center, radius, 0.0f, a_max, num_segments - 1);
+        else path_arc_to(center, radius, 0.0f, (std::numbers::pi * 2.f) * (num_segments - 1.f) / num_segments, num_segments - 1);
         path_fill_convex(clr);
     }
 }
