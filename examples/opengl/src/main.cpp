def72253bb0d35621e1cd0e507e1cf39653d4bee
suddenly <suddenlybydl0@gmail.com>
2022-12-28 10:34:26 +0500

    Added opengl example.

---
+++ examples/opengl/src/main.cpp
@@ -0,0 +1,64 @@
+﻿#include <iostream>
+#define null_renderer_use_glfw
+#include <null-renderer-opengl3.h>
+
+null::renderer::c_window window{ };
+null::render::c_draw_list custom_layer{ };
+
+void main_loop() {
+	static const null::render::e_text_flags text_flags{ null::render::e_text_flags::aligin_bottom | null::render::e_text_flags::aligin_center_x };
+	static const null::render::e_corner_flags corner_flags{ null::render::e_corner_flags::bot_left | null::render::e_corner_flags::top_right };
+
+	static const color_t color{ 100, 100, 255 };
+
+	static const null::render::multicolor_text_t<std::string> multicolor_text{ {
+		{ "multicolored \n\n", { } },
+		{ "text", color }
+	} };
+
+	null::render::begin_frame(window); {
+		custom_layer.draw_text("text drawed by custom draw_list", { 100, 10 }, { });
+		null::render::background.draw_text(std::format("[ opengl ] fps: {:3.0f}", 1.f / window.time_data.delta_time), { window.get_window_size().x, 10.f }, { }, null::render::e_text_flags{ -null::render::e_text_flags::aligin_right | -null::render::e_text_flags::aligin_center_y | -null::render::e_text_flags::outline });
+		null::render::background.draw_text(multicolor_text, { 10 });
+
+		null::render::background.draw_text("rect filled", { 100, 200 }, { }, text_flags);
+		null::render::background.draw_rect_filled({ 50, 200 }, { 150, 300 }, color, 10.f, corner_flags);
+
+		null::render::background.draw_text("rect", { 250, 200 }, { }, text_flags);
+		null::render::background.draw_rect({ 200 }, { 300 }, color, 2.f, 10.f, corner_flags);
+
+		null::render::background.draw_text("circle filled", { 400, 200 }, { }, text_flags);
+		null::render::background.draw_circle_filled({ 400, 250 }, color, 50);
+
+		null::render::background.draw_text("circle", { 550, 200 }, { }, text_flags);
+		null::render::background.draw_circle({ 550, 250 }, color, 50, 0, 2.f);
+
+		null::render::background.draw_text("multicolor rect filled", { 100, 400 }, { }, text_flags);
+		null::render::background.draw_rect_filled_multicolor({ 50, 400 }, { 150, 500 }, { color_t<int>::palette_t::red, color_t<int>::palette_t::green, color_t<int>::palette_t::blue, color_t<int>::palette_t::white }, 10.f, corner_flags);
+
+		null::render::background.draw_text("multicolor rect", { 250, 400 }, { }, text_flags);
+		null::render::background.draw_rect_multicolor({ 200, 400 }, { 300, 500 }, { color_t<int>::palette_t::red, color_t<int>::palette_t::green, color_t<int>::palette_t::blue, color_t<int>::palette_t::white }, 2.f, 10.f, corner_flags);
+
+		null::render::background.draw_poly_line({ { 400 }, { 550, 400 }, { 550, 500 } }, { }, true);
+
+		//@note:	you can use L"" to display cyrillic
+		//			or you can add /utf - 8 to C/C++->command line
+		null::render::background.draw_text("абвгдеёжзийклмнопрстуфхцчшщъыьэюя", { 500 }, { });
+	} null::render::end_frame();
+}
+
+int main() {
+	window.callbacks.at<utils::win::e_window_callbacks::on_main_loop>().add(main_loop);
+
+	try {
+		null::render::atlas.add_font_from_file_ttf("C:\\Windows\\fonts\\Tahoma.ttf", 13.f, nullptr, null::render::c_font::glyph_t::ranges_cyrillic());
+
+		window.create();
+		null::render::custom_lists.push_back(&custom_layer);
+
+		window.main_loop();
+		window.destroy();
+	} catch(const std::exception& exception) {
+		std::cout << exception.what() << std::endl;
+	}
+}
--- src/renderers/null-renderer-opengl3/src/null-renderer-opengl3.h
+++ src/renderers/null-renderer-opengl3/src/null-renderer-opengl3.h
@@ -1,4 +1,10 @@
 #pragma once
+#ifdef null_renderer_use_glfw
+#define GLFW_EXPOSE_NATIVE_WIN32
+#include <GLFW/glfw3.h>
+#include <GLFW/glfw3native.h>
+#endif
+
 #include <opengl/opengl3.h>
 
 #include <null-render.h>
@@ -23,4 +29,60 @@ namespace null::renderer {
 	void destroy_device_objects();
 
 	static void begin_frame() { if(!shader_program) create_device_objects(); }
+
+#ifdef null_renderer_use_glfw
+	class c_window : public utils::win::c_window {
+	public: using utils::win::c_window::c_window;
+		color_t<float> clear_color{ 0.07f, 0.07f, 0.07f };
+		GLFWwindow* glfw_window{ };
+
+	public:
+		bool create() override {
+			if(!glfwInit()) throw std::runtime_error{ "cant init glfw" };
+
+			glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+			glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+
+			if(!(glfw_window = glfwCreateWindow(size.x, size.y, name.c_str(), NULL, NULL))) throw std::runtime_error{ "cant create glfw window" };
+			glfwSetWindowPos(glfw_window, pos.x, pos.y);
+			glfwMakeContextCurrent(glfw_window);
+			glfwSwapInterval(1);
+
+			wnd_handle = glfwGetWin32Window(glfw_window);
+
+			on_create();
+			return true;
+		}
+
+		void destroy() override {
+			on_destroy();
+			glfwDestroyWindow(glfw_window);
+			glfwTerminate();
+		}
+
+		void main_loop() override {
+			if(!wnd_handle) throw std::runtime_error{ "window handle is nullptr" };
+
+			while(!glfwWindowShouldClose(glfw_window)) {
+				glfwPollEvents();
+
+				time_data.begin_frame();
+				begin_frame();
+
+				on_main_loop();
+
+				setup_default_draw_data();
+
+				sdk::i_vec2<int> framebuffer{ };
+				glfwGetFramebufferSize(glfw_window, &framebuffer.x, &framebuffer.y);
+				glViewport(0, 0, framebuffer.x, framebuffer.y);
+				glClearColor(clear_color.r(), clear_color.g(), clear_color.b(), clear_color.a());
+				glClear(GL_COLOR_BUFFER_BIT);
+				render();
+
+				glfwSwapBuffers(glfw_window);
+			}
+		}
+	};
+#endif
 }
